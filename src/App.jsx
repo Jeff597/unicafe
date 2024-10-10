3f5b288d283c687a173d817d6d64c1aa67e5ead3
jeff597 <u1011jeff@gmail.com>
2024-10-10 17:57:36 -0500

    1.10: unicafe step 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,75 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './main.scss'
 
 const All = (props) => {
-  const {setTotal} = props
-  const value = Object.values(props).reduce((acc, val) => acc + (typeof val === 'number' ? val : 0), 0);
-  setTotal(value)
-  return <p>All {value}</p>
+
 }
 
 const Average = (props) => {
-  const {good, bad, total} = props
+  const { good, bad, total } = props
   return <p>Average {total > 0 ? (good - bad) / total : 0}</p>
 }
 
 const Positive = (props) => {
-  const {good, total} = props
+  const { good, total } = props
   return <p>Positive {total > 0 ? (good / total) * 100 : 0}</p>
 }
 
-const Button = ({handleClick, text}) => {
+const Button = ({ handleClick, text }) => {
+  console.log("click")
   return <button onClick={handleClick}>{text}</button>
 }
 
+const StatisticLine = (props) => {
+  const {text, value} = props
+
+switch (text) {
+  case "good":
+    return <p>good {value}</p>
+    break;
+  case "neutral":
+    return <p>neutral {value}</p>
+    break;
+  case "bad":
+    return <p>bad {value}</p>
+    break;
+  case "all":
+    return <p>all {value}</p>
+    break;  
+  case "average":
+    return <p>average {value}</p>
+    break;  
+  case "positive":
+    return <p>positive {value}</p>
+    break;  
+  default:
+    return <></>
+    break;
+}
+}
+
 const Statistics = (props) => {
-  const {good, bad, total} = props
-   if(good || bad > 0) {return <div className="statistics">
-  <p>Average {total > 0 ? (good - bad) / total : 0}</p>
-  <p>Positive {total > 0 ? (good / total) * 100 : 0}</p>
-  </div>} else {
+  const { good, bad, neutral, total, setTotal } = props
+  const values = {good, bad, neutral}
+  const all = Object.values(values).reduce((acc, val) => acc + (typeof val === 'number' ? val : 0), 0);
+
+  useEffect(() => {
+     setTotal(all)
+  })
+  
+  if (good || bad || neutral > 0) {
+    return  <div className="statistics">
+              <h2>statistics</h2>
+              <StatisticLine text="good" value={good}/>
+              <StatisticLine text="neutral" value={neutral}/>
+              <StatisticLine text="bad" value={bad}/>
+              <StatisticLine text="all" value={all}/>
+              <StatisticLine text="average" value={total > 0 ? (good - bad) / total : 0}/>
+              <StatisticLine text="positive" value={total > 0 ? (good / total) * 100 : 0}/>
+
+            </div>
+  }
+  else {
     return "No feedback given"
   }
 
@@ -49,21 +91,12 @@ const App = () => {
 
       <div className='grid'>
 
-      <Button text="good" handleClick={() => setGood(good + 1)}/>
-      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)}/>
-      <Button text="bad" handleClick={() => setBad(bad + 1)}/>
+        <Button text="good" handleClick={() => setGood(good + 1)} />
+        <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+        <Button text="bad" handleClick={() => setBad(bad + 1)} />
       </div>
 
-
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <All good={good} bad={bad} neutral={neutral}
-      
-      
-      setTotal={setTotal}/> 
-      <Statistics good={good} bad={bad} total={total}/>
+      <Statistics good={good} bad={bad} neutral={neutral} total={total} setTotal={setTotal} />
     </main>
   )
 }
