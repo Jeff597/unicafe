b2c5d21f3afcfa341db128930b24685b7596939c
jeff597 <u1011jeff@gmail.com>
2024-10-13 18:56:52 -0500

    1.11*: unicafe step 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,115 @@
 import { useEffect, useState } from 'react'
 import './main.scss'
 
-const All = (props) => {
-
-}
-
-const Average = (props) => {
-  const { good, bad, total } = props
-  return <p>Average {total > 0 ? (good - bad) / total : 0}</p>
-}
-
-const Positive = (props) => {
-  const { good, total } = props
-  return <p>Positive {total > 0 ? (good / total) * 100 : 0}</p>
-}
 
 const Button = ({ handleClick, text }) => {
-  console.log("click")
+
   return <button onClick={handleClick}>{text}</button>
 }
 
-const StatisticLine = (props) => {
-  const {text, value} = props
-
-switch (text) {
-  case "good":
-    return <p>good {value}</p>
-    break;
-  case "neutral":
-    return <p>neutral {value}</p>
-    break;
-  case "bad":
-    return <p>bad {value}</p>
-    break;
-  case "all":
-    return <p>all {value}</p>
-    break;  
-  case "average":
-    return <p>average {value}</p>
-    break;  
-  case "positive":
-    return <p>positive {value}</p>
-    break;  
-  default:
-    return <></>
-    break;
+const Col1 = ({ text }) => {
+  return <td>{text}</td>
 }
+
+const Col2 = ({ value }) => {
+  return <td>{value}</td>
+}
+
+const StatisticLine = (props) => {
+  const { text, value } = props
+
+
+  switch (text) {
+    case "good":
+      return <>
+        <Col1 text={text} />
+        <Col2 value={value} />
+      </>
+
+      break;
+    case "neutral":
+      return <>
+        <Col1 text={text} />
+        <Col2 value={value} />
+      </>
+      break;
+    case "bad":
+      return <>
+        <Col1 text={text} />
+        <Col2 value={value} />
+      </>
+      break;
+    case "all":
+      return <>
+        <Col1 text={text} />
+        <Col2 value={value} />
+      </>
+      break;
+    case "average":
+      return <>
+        <Col1 text={text} />
+        <Col2 value={value} />
+      </>
+      break;
+    case "positive":
+      return <>
+        <Col1 text={text} />
+        <Col2 value={value} />
+      </>
+      break;
+    default:
+      return <></>
+      break;
+  }
+
+
+
+
+
 }
 
 const Statistics = (props) => {
   const { good, bad, neutral, total, setTotal } = props
-  const values = {good, bad, neutral}
+  const values = { good, bad, neutral }
   const all = Object.values(values).reduce((acc, val) => acc + (typeof val === 'number' ? val : 0), 0);
 
   useEffect(() => {
-     setTotal(all)
+    setTotal(all)
   })
-  
-  if (good || bad || neutral > 0) {
-    return  <div className="statistics">
-              <h2>statistics</h2>
-              <StatisticLine text="good" value={good}/>
-              <StatisticLine text="neutral" value={neutral}/>
-              <StatisticLine text="bad" value={bad}/>
-              <StatisticLine text="all" value={all}/>
-              <StatisticLine text="average" value={total > 0 ? (good - bad) / total : 0}/>
-              <StatisticLine text="positive" value={total > 0 ? (good / total) * 100 : 0}/>
 
-            </div>
+  if (good || bad || neutral > 0) {
+    return <table className="statistics">
+
+      <tbody>
+        <tr>
+          <StatisticLine text="good" value={good} />
+        </tr>
+        <tr>
+          <StatisticLine text="neutral" value={neutral} />
+        </tr>
+        <tr>
+          <StatisticLine text="bad" value={bad} />
+        </tr>
+        <tr>
+          <StatisticLine text="all" value={all} />
+        </tr>
+        <tr>
+          <StatisticLine text="average" value={total > 0 ? (good - bad) / total : 0} />
+        </tr>
+        <tr>
+          <StatisticLine text="positive" value={total > 0 ? (good / total) * 100 : 0} />
+        </tr>
+      </tbody>
+
+
+
+
+    </table>
+
+
+
+
+
   }
   else {
     return "No feedback given"
@@ -95,7 +137,7 @@ const App = () => {
         <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
         <Button text="bad" handleClick={() => setBad(bad + 1)} />
       </div>
-
+      <h2>statistics</h2>
       <Statistics good={good} bad={bad} neutral={neutral} total={total} setTotal={setTotal} />
     </main>
   )
